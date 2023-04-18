82094d5e661299b8a54dd2c4082c463d844e2187
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-04-18 17:42:28 +0200

    Result from image manipulator is not readable

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Button, Image, StyleSheet, View } from "react-native";
+import * as FileSystem from "expo-file-system";
 import { Asset } from "expo-asset";
 import { manipulateAsync, FlipType, SaveFormat } from "expo-image-manipulator";
 
@@ -9,6 +10,14 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
+      const cache = await FileSystem.readDirectoryAsync(
+        FileSystem.cacheDirectory
+      );
+      const document = await FileSystem.readDirectoryAsync(
+        FileSystem.documentDirectory
+      );
+      console.log("cache", cache);
+      console.log("document", document);
       const image = Asset.fromModule(require("./assets/icon.png"));
       await image.downloadAsync();
       setImage(image);
@@ -23,6 +32,10 @@ export default function App() {
       { compress: 1, format: SaveFormat.PNG }
     );
     console.log(manipResult);
+    await FileSystem.copyAsync({
+      from: manipResult.uri,
+      to: FileSystem.documentDirectory,
+    });
     setImage(manipResult);
   };
 
